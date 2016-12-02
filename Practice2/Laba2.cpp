471a13345cc01f99ec2af051ba4b2c98143fcb6d
Trpoal <Trpoal@mail.ru>
2016-12-02 21:52:22 +0300

    C

---
+++ Practice2/Laba2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    printf("Guess the number\n");
+    srand(time(NULL));
+    int chislo = 0 + rand() % 100;
+    int i = 1; 
+    int ch;
+    while (i != 0)
+    {
+        printf("Enter the number\n");
+        scanf("%d", &ch);
+        if (ch == chislo)
+        {
+            i = 0;
+            printf("WOW\n");
+        }
+        if (ch > chislo)
+            printf("Less\n");
+        if (ch < chislo)
+            printf("More\n");
+    }
+}

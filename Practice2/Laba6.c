457638c2dc061a249c21e3e26341e9ca748fe0d1
Trpoal <Trpoal@mail.ru>
2016-12-03 00:16:49 +0300

    Create Laba6

---
+++ Practice2/Laba6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    char str[50];
+    printf("Enter string\n");
+    gets(str);
+    int len = strlen(str);
+    printf("%d\n", len);
+    while (str[0] == ' ')
+    {
+        for (int i = 0; i <= len; i++)
+            str[i] = str[i + 1];
+        len = len - 1;
+    }
+    while (str[len-1] == ' ')
+    {
+        len = len - 1;
+    }
+    for (int i = 1; i <= len-2; i++)
+    {
+        while (str[i] == ' '&&str[i + 1] == ' ')
+        {
+                for (int j = i; j <= len; j++)
+                   str[j] = str[j + 1];
+                len = len - 1;
+        }
+        
+    }
+    printf("%s\n", str);
+}

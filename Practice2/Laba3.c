de5f211c35efbad9d01a48767603af3037839420
Trpoal <Trpoal@mail.ru>
2016-12-02 22:30:21 +0300

    C

--- Practice2/Laba3.c
+++ Practice2/Laba3.c
@@ -5,14 +5,19 @@ int main()
     printf("Enter\n");
     scanf("%d", &str);
     int probel = (1+(str-1)*2) / 2;
+
     for (int i = 1; i <= str; i++)
     {
         star = 1 + (i - 1) * 2;
+
         for (int k = probel; k >= 0; k--)
             printf(" ");
+
         for (int j = 1; j <= star; j++)
             printf("*");
+
         printf("\n");
         probel= probel - 1;
     }  
+
 }

f1ce3e36d664998b2e50655dfc13a309017f2478
Trpoal <Trpoal@mail.ru>
2016-12-02 20:34:16 +0300

    C

---
+++ Practice1/Laba4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float s;
+    char k;
+    printf("Enter R for translation from radians to degrees\n");
+    printf("Enter D for translation from degrees to radians\n");
+    scanf("%f%c", &s, &k);
+    if (k == 'D')
+    {
+        s = (s*3.14) / 180;
+        printf("%f radians\n", s);
+    }
+    
+    if (k == 'R')
+    {
+        s = (s * 180) / 3.14;
+        printf("%f degrees\n", s);
+    }
+    
+    return(0);
+}

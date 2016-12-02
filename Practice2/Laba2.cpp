3cbaecb40e945e10d346f8b7c23fda8f17a4df51
Trpoal <Trpoal@mail.ru>
2016-12-02 22:31:02 +0300

    C

--- Practice2/Laba2.cpp
+++ Practice2/Laba2.cpp
@@ -8,18 +8,23 @@ int main()
     int chislo = 0 + rand() % 100;
     int i = 1; 
     int ch;
+
     while (i != 0)
     {
         printf("Enter the number\n");
         scanf("%d", &ch);
+
         if (ch == chislo)
         {
             i = 0;
             printf("WOW\n");
         }
+
         if (ch > chislo)
             printf("Less\n");
+
         if (ch < chislo)
             printf("More\n");
+
     }
 }

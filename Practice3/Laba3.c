c60e38b84f953226a6e5bc2f507b8e905305f7c6
Trpoal <Trpoal@mail.ru>
2016-12-09 22:24:12 +0300

    Update Laba3.c

--- Practice3/Laba3.c
+++ Practice3/Laba3.c
@@ -1,18 +1,21 @@
 #include <stdio.h>;
-#define SIZE 20
+#define SIZE 100
 
 int main()
 {
     char str[SIZE];
+
     printf("Enter\n");
     gets(str);
+
     int slova=0;
+
     for (int i = 0; str[i] != '\0'; i++)
     {
-
         if ((str[i] != ' ') && ((str[i + 1] = ' ')||(str[i+1]=='\0')))
             slova++;
     }
+
     printf("%d\n", slova);
 }
 

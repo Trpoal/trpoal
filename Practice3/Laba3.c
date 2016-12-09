ae8b9ba01dcc7fce9163e0aeb304b5d19862fc05
Trpoal <trpoal@mail.ru>
2016-12-09 22:42:11 +0300

    Update Laba3.c

--- Practice3/Laba3.c
+++ Practice3/Laba3.c
@@ -12,7 +12,7 @@ int main()
 
     for (int i = 0; str[i] != '\0'; i++)
     {
-        if ((str[i] != ' ') && ((str[i + 1] = ' ')||(str[i+1]=='\0')))
+        if ((str[i] != ' ') && ((str[i + 1] == ' ')||(str[i+1]=='\0')))
             slova++;
     }
 
